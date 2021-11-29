d739a897af38ac71258f9f3e5c5cbc602f501dfc
MisterEnderman <spongewent1@gmail.com>
2021-11-29 22:17:16 +0200

    fixed a bug where a command will run twice.

--- src/main/java/me/itamar/plugins/mcutils/ingame/CommandUtility.java
+++ src/main/java/me/itamar/plugins/mcutils/ingame/CommandUtility.java
@@ -47,6 +47,7 @@ public class CommandUtility {
                 }
 
                 execute((Player) sender, args);
+                return true;
             }
 
             execute(sender, args);
